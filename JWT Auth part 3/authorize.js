4037931f1f6fd4a887833568d7dda0c67f0f0865
Andros924 <sir_vivi@hotmail.it>
2024-09-12 09:14:07 +0200

    esercizi JWT

---
+++ JWT Auth part 3/authorize.js
@@ -0,0 +1,10 @@
+const authorize = passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    if (err || !user) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    req.user = user; 
+    next();
+  });
+  
+  module.exports = authorize;
+  
